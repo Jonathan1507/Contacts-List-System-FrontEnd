f32cea41e0e0af6a4c3f4eabf9c290b67b05ab2e
Jonathan <jonathanvasquez96@hotmail.com>
2020-10-23 02:06:02 -0500

    y

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './pages/maintenance/contacts/contacts.component';
+import { UsersComponent } from './pages/maintenance/users/users.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    UsersComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
